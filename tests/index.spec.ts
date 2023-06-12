cce3a755bc04007363e60df418b2a11b0f7fb84c
Ubu <ubald2b@gmail.com>
2023-06-12 14:41:01 +0200

    refactoring

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -5,7 +5,7 @@ import * as Utils from "../src/utils";
 describe('Orders', function () {
 
     test('should return amount - discount as total amount when order is web and total amount <= 50', () => {
-        const type: string = "WEB";
+        const type = "WEB";
         const order: Order = new Order(type);
 
         const getAmountSpy = jest.spyOn(Utils, 'getAmount');
@@ -17,7 +17,7 @@ describe('Orders', function () {
         expect(totalAmount).toEqual(50)
     })
     test('should add a -1 discount on web order when total amount is > 50', () => {
-        const type: string = "WEB";
+        const type = "WEB";
         const order: Order = new Order(type);
 
         const getAmountSpy = jest.spyOn(Utils, 'getAmount');
@@ -30,7 +30,7 @@ describe('Orders', function () {
     })
 
     test('should add a -10 discount on web order when total amount is >= 70', () => {
-        const type: string = "WEB";
+        const type = "WEB";
         const order: Order = new Order(type);
 
         const getAmountSpy = jest.spyOn(Utils, 'getAmount');
@@ -42,7 +42,7 @@ describe('Orders', function () {
         expect(totalAmount).toEqual(60)
     })
     test('should return a total amount between 10 and 100 when order is retail', () => {
-        const type: string = "RETAIL";
+        const type = "RETAIL";
         const order: Order = new Order(type);
 
         const totalAmount = order.getTotalAmount();
@@ -50,32 +50,32 @@ describe('Orders', function () {
         expect(totalAmount).toBeLessThanOrEqual(100)
     })
     test('should return 100 as total amount when order is from a market place', () => {
-        const type: string = "MARKET_PLACE";
+        const type = "MARKET_PLACE";
         const order: Order = new Order(type);
 
         const totalAmount = order.getTotalAmount();
         expect(totalAmount).toEqual(100)
     })
     test('should throw error if order type is not supported', () => {
-        const type: string = "WRONG_ORDER_TYPE";
+        const type = "WRONG_ORDER_TYPE";
         const order: Order = new Order(type);
 
         expect(() => order.getTotalAmount()).toThrowError();
     })
     test('should have default order number if it was not initialized', () => {
-        const type: string = "WRONG_ORDER_TYPE";
+        const type = "WRONG_ORDER_TYPE";
         const order: Order = new Order(type);
 
         expect(order.orderNumber).toEqual("defaultOrderNumber")
     })
     test('should have an order number if it when well initialized', () => {
-        const type: string = "WRONG_ORDER_TYPE";
+        const type = "WRONG_ORDER_TYPE";
         const order: Order = new Order(type, "123ABC456");
 
         expect(order.orderNumber).toEqual("123ABC456")
     })
     test('should display Market-Place: before order number for Market Place order', () => {
-        const type: string = "MARKET_PLACE";
+        const type = "MARKET_PLACE";
         const order: Order = new Order(type, "123ABC456");
 
         expect(order.getOrderNumber()).toEqual("Market-Place:123ABC456")
